807a3ef086d47e4a002b956ad24f842b90abb70a
Jelle Maas <typiqally@gmail.com>
2022-03-16 11:18:18 +0100

    Initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Election {
+    string candidateName;
+
+    constructor() {
+        candidateName = "Candidate 1";
+    }
+
+    function getCandidate() public view returns (string memory) {
+        return candidateName;
+    }
+
+    function setCandidate (string memory name) public {
+        candidateName = name;
+    }
+}
