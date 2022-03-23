9fdfc4746eed9505812b184266ee5678d6d30d9a
Jelle Maas <typiqally@gmail.com>
2022-03-23 10:44:56 +0100

    Change deploy script to upload contract address and artifact to the frontend

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,9 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { artifacts, ethers } from "hardhat";
+import * as path from "path";
+import { Contract } from "ethers";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -26,6 +28,28 @@ async function main() {
   await election.deployed();
 
   console.log("Election contract address:", election.address);
+
+  saveContract("Election", election);
+}
+
+function saveContract(name: string, contract: Contract) {
+  const fs = require("fs");
+  const contractsDir = path.join(__dirname, "/../frontend/contracts/");
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  const artifact = artifacts.readArtifactSync(name);
+  const contractFile = {
+    address: contract.address,
+    artifact: artifact,
+  };
+
+  fs.writeFileSync(
+    contractsDir + `/${name}.json`,
+    JSON.stringify(contractFile, null, 2)
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
