1c608d9ff9400f86a76a80d51d801027c8516ca6
Jelle Maas <typiqally@gmail.com>
2022-03-23 19:35:39 +0100

    Cleanup Election contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -5,69 +5,53 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Election {
 
     struct Candidate {
-        uint id;
-        address addr;
+        address ownAddress;
         string name;
         uint votes;
     }
 
-    struct Ballot {
-        uint id;
-        bool voted;
-        address voter;
-        address candidate;
+    struct Vote {
+        address ownAddress;
+        address candidateAddress;
     }
 
     address[] public candidateAddresses;
-    uint candidateAddressCount;
-    uint voteCount;
 
     mapping(address => Candidate) public candidates;
-    mapping(address => Ballot) public ballot;
+    mapping(address => Vote) public ballot;
 
     function getCandidateAddresses() public view returns (address[] memory) {
         return candidateAddresses;
     }
 
     function signUpForCandidate(string memory name) public {
-        require(!validCandidate(msg.sender));
+        require(!candidateExists(msg.sender));
+
+        candidates[msg.sender] = Candidate(msg.sender, name, 0);
         candidateAddresses.push(msg.sender);
-        candidateAddressCount++;
-        candidates[msg.sender] = Candidate(candidateAddressCount, msg.sender, name, 0);
     }
 
-    function castVote(address addr) public {
-        if(checkBallot(msg.sender) == false){
-            require(validCandidate(addr));
-            candidates[addr].votes += 1;
-            voteCount++;
-            ballot[msg.sender] = Ballot(voteCount, true, msg.sender, addr);
-        }
+    function castVote(address _address) public {
+        require(ballotExists(msg.sender));
+
+        require(candidateExists(_address));
+        candidates[_address].votes += 1;
+        ballot[msg.sender] = Vote(msg.sender, _address);
     }
 
-    function getCandidate(address addr) public view returns (Candidate memory) {
-        return candidates[addr];
-    }
-
-    function validCandidate(address addr) public view returns (bool) {
-        if(candidates[addr].id != 0) {
+    function candidateExists(address _address) public view returns (bool) {
+        if (candidates[_address].ownAddress != address(0)) {
             return true;
         }
+
         return false;
     }
 
-    function checkBallot(address addr) public view returns (bool) {
-        if(ballot[addr].id != 0) {
+    function ballotExists(address _address) public view returns (bool) {
+        if (ballot[_address].ownAddress != address(0)) {
             return true;
         }
+
         return false;
-    }
-
-    function getCount() public view returns (uint) {
-        return candidateAddressCount;
-    }
-
-    function getTotalVotes(address addr) public view returns (uint) {
-        return candidates[addr].votes;
     }
 }
